41c7539eac0024b80d1fd10c51336d755d3a6727
Sykona <oliverremy@yahoo.de>
2015-10-16 17:53:41 +0200

    Revert "Revert "Revert "JavaDoc Kommentare"""
    This reverts commit d1416a2243b3dcb8ed9106d33da118d6ef7ae824.

--- SoftwareEngineering/src/Stack.java
+++ SoftwareEngineering/src/Stack.java
@@ -1,29 +1,9 @@
-/**
- * Interface for the standard operations on a LIFO
- * ("Last in, first out") stack.
- * 
- * @author Oliver Remy
- * @author Sebastian Strumegger
- * 
- * @param <T> generic type argument T
- */
+
 public interface Stack<T>{
 	
-	/**
-	 * Returns the current amount of items on the stack.
-	 * @return amount of items on the stack
-	 */
 	public int getStackSize();
 	
-	/**
-	 * Pushes the item on the stack.
-	 * @param item to be pushed on the stack
-	 */
 	public void push (T item);
 	
-	/**
-	 * Pops the first (the top) item off the stack.
-	 * @return first item on the stack
-	 */
 	public T pop ();
 }
