5f71296b1f15846a67a3ccda960e990c4f2d41c4
Sebastian Strumegger <s10131191@stud.sbg.ac.at>
2015-11-03 11:59:34 +0100

    Reconstruct

---
+++ SE_A2/src/test.java
@@ -0,0 +1,22 @@
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class test {
+
+	public static <T> void main(String[] args) throws FileNotFoundException, ClassNotFoundException {
+
+		Class<ArrayList> ArrayListDummy = ArrayList.class;
+		Reconstructor re = new Reconstructor();
+		
+		String linkedListClass = LinkedList.class.getCanonicalName();
+		
+		re.reconstruct(ArrayListDummy, new PrintStream("ArrayListDummy.java"));
+		re.reconstruct(linkedListClass, new PrintStream("LinkedListDummy.java"));
+		
+		
+
+	}
+
+}
