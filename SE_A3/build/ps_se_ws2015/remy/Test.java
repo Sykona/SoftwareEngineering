3dbcdeb7173b6796765f4080defd810a35690efd
Sebastian Strumegger <s10131191@stud.sbg.ac.at>
2015-12-17 14:12:02 +0100

    Antscript und Javadoc ergänzt... könnma abgeben

---
+++ SE_A3/build/ps_se_ws2015/remy/Test.java
@@ -0,0 +1,7 @@
+package ps_se_ws2015.remy;
+
+public class Test {
+	public static void main (String[] args) throws Exception {
+		new Reconstructor().reconstruct(Class.forName("java.util.ArrayList"), System.out);
+	}
+}
