6f2752bf6933759d29c878f25292bfa7aabb5709
Oliver Remy <oliverremy@yahoo.de>
2015-12-21 09:47:23 +0100

    asdf

--- SE_A3/build/ps_se_ws2015/remy/Test.java
+++
@@ -1,7 +0,0 @@
-package ps_se_ws2015.remy;
-
-public class Test {
-	public static void main (String[] args) throws Exception {
-		new Reconstructor().reconstruct(Class.forName("java.util.ArrayList"), System.out);
-	}
-}
